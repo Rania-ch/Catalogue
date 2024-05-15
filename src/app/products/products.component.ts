f741aa4c6b3a76e9d687f9b6fc93a93b7a27721f
Rania Chaieb <rania23chaieb@gmail.com>
2024-05-15 19:53:05 +0100

    second commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Product } from '../model/product.model';
+import { ProductService } from './../services/product.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-products',
@@ -6,20 +10,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  products! : Array<any>;
 
-  constructor() { }
+
+
+  products! : Array<Product>;
+  errorMessage! : string;
+  searchFormGroup! : FormGroup;
+  currentPage : number =0;
+  pageSize:number =5;
+  totalPages : number=0;
+  currentAction :string="all"
+  constructor(private ProductService : ProductService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.products=[
-      {id:1,name:"Computer",price:6500},
-      {id:2,name:"Printer",price:1200},
-      {id:3,name:"Smart phone",price:1400},
-    ]
+ 
+  this.searchFormGroup=this.fb.group({
+    Keyword : this.fb.control(null)
+   
+   
+  });
+  this.handleGetPageProducts();
   }
-  handleDeleteProduct(p :any){
-    let index = this.products.indexOf(p);
-    this.products.splice(index,1)
+  handleGetAllProducts(){
+    this.ProductService.getAllProducts().subscribe({
+      next : (data) => {
+       this.products=data;
+      },
+      error : (err) =>{
+       this.errorMessage=err;
+      }
+     });
   }
-
+  handleDeleteProduct(p :Product){
+    let conf =confirm("Are you sure?")
+    if (conf==false) return;
+     this.ProductService.deleteProduct(p.id).subscribe({
+      next :(data) =>{
+        // this.handleGetAllProducts()
+        let index =this.products.indexOf(p);
+        this.products.splice(index,1);
+      }
+     })
+  }
+  handleSetPromotion(p: Product) {
+    let promo=p.promotion;
+   this.ProductService.setPromotion(p.id).subscribe({
+    next :(data) =>{
+      p.promotion=!promo;
+    }, 
+    error : err=> {this.errorMessage=err;}
+        
+   
+   })
+    }
+    handleSearchProducts() {
+      this.currentAction="search";
+     
+      let keyword=this.searchFormGroup.value.Keyword;
+      this.ProductService.searchProducts(keyword,this.currentPage,this.pageSize).subscribe(
+        {
+          next : (data) =>{
+            this.products=data.products;
+            this.totalPages=data.totalPages;
+          }
+        }
+      )
+    }
+    handleGetPageProducts(){
+      this.ProductService.getPageProducts(this.currentPage,this.pageSize).subscribe({
+        next : (data) => {
+         this.products=data.products;
+         this.totalPages=data.totalPages;
+        },
+        error : (err) =>{
+         this.errorMessage=err;
+        }
+       });
+    }
+    gotoPage(i: number) {
+      this.currentPage=i;
+      if(this.currentAction==='all')
+      this.handleGetPageProducts();
+    else
+      this.handleSearchProducts();
+      }
 }
